00ec0a8b0b2f40b487deda1150b0797126df1d55
Justin <jiabin.huang@unisco.com>
2024-10-28 13:41:42 +0800

    init

---
+++ lt/array/LT704.java
@@ -0,0 +1,12 @@
+package lt.array;
+
+/**
+ * @author: Justin Huang
+ * @description:
+ * @date: 2024/10/28 13:39
+ */
+public class LT704 {
+    public int search(int[] nums, int target) {
+        return 0;
+    }
+}
