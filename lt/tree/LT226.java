3a5ae5640d46983066d136cc069c6060313bb045
Justin <jiabin.huang@unisco.com>
2024-10-31 17:09:33 +0800

    226 用stack替代递归的解法

--- lt/tree/LT226.java
+++ lt/tree/LT226.java
@@ -3,13 +3,36 @@ package lt.tree;
 
 import lt.struct.TreeNode;
 
+import java.util.Stack;
+
 /**
  * @author: Justin Huang
  * @description:
  * @date: 2024/10/30 16:35
  */
 public class LT226 {
+
+
     public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            swapLeftAndRight(node);
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+            }
+        }
+        return root;
+    }
+
+    public TreeNode invertTree_Recursion(TreeNode root) {
         if (root == null) {
             return null;
         }
